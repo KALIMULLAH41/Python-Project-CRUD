e5717e99e8124bd4b3e36bf4a05377f6e7a05fc6
KALIMULLAH41 <kalimullah41557@gmail.com>
2024-11-07 03:25:47 +0530

    add a feature Add Item button

--- FoodApp/urls.py
+++ FoodApp/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 app_name='FoodApp'
 urlpatterns = [
     path('',views.home,name='home'),
-    path('<int:item_id>/',views.details,name='details')
+    path('<int:item_id>/',views.details,name='details'),
+    path('add/',views.add_item,name='additem'),
 ]
+
--- FoodApp/views.py
+++ FoodApp/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Items
+from FoodApp.forms import ItemForm
+
+
 # Create your views here.
 def home(request):
     item_list=Items.objects.all()
@@ -14,3 +17,11 @@ def details(request,item_id):
         'item_list':item_list
     }
     return render(request,'FoodApp/details.html',context)
+
+def add_item(request):
+    form=ItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('FoodApp:home')
+        
+    return render(request,'FoodApp/item-form.html',{'form':form})
